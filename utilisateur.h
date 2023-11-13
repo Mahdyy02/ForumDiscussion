e357c3f7fc6abcb2dcf77d7bff80116a5d93b3b3
cutiegirl02 <150730195+cutiegirl02@users.noreply.github.com>
2023-11-13 22:00:06 +0100

    Add files via upload

--- utilisateur.h
+++ utilisateur.h
@@ -1,5 +1,7 @@
 #ifndef UTILISATEUR_H_INCLUDED
 #define UTILISATEUR_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
 
 
 typedef struct 
@@ -16,6 +18,45 @@ typedef struct
     unsigned short int Administrateur;
 
 }UTILISATEUR;
+
+void inscription(UTILISATEUR* u){
+    printf("Nom: ");
+    u->Nom = (char*) malloc(sizeof(char));
+    scanf("%s", &(u->Nom));
+    printf("Prenom: ");
+    u->Prenom = (char*) malloc(sizeof(char));
+    scanf("%s",&(u->Prenom));
+    printf("Adresse: ");
+    u->Adresse = (char*) malloc(sizeof(char));
+    scanf("%s",&(u->Adresse));
+    printf("Date de naissance: ");
+    u->Date_de_naissance = (char*) malloc(sizeof(char));
+    scanf("%s",&(u->Date_de_naissance));
+    printf("Numero de telephone: ");
+    scanf("%i", &(u->Numero_telephone));
+    printf("Adresse_email: ");
+    scanf("%i",&(u->Adresse_email));
+    printf("Password:");
+    
+
+
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+    
+}
+
 
 
 
