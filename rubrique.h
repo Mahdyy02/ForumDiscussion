0a079ae912e6bcb4e78902737e3e4955f670c44f
cutiegirl02 <150730195+cutiegirl02@users.noreply.github.com>
2023-11-13 21:10:48 +0100

    Add files via upload
    c est la structure rubrique

---
+++ rubrique.h
@@ -0,0 +1,13 @@
+#ifndef RUBRIQUE_H_INCLUDED
+#define RUBRIQUE_H_INCLUDED
+
+typedef struct 
+{
+    char* Nom ;
+    char* Date_de_poste;
+    unsigned int Numero_inscription;
+    char* Message;
+    char* Liste_internet;
+
+} RUBRIQUE;
+#endif
