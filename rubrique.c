0087a9b8ece7d1a93d8e43667764392e5ec2ac54
cutiegirl02 <150730195+cutiegirl02@users.noreply.github.com>
2023-11-16 19:13:11 +0100

    Add files via upload
    j'ai changé la structure date

--- rubrique.c
+++ rubrique.c
@@ -2,7 +2,7 @@
 
 void affichage(RUBRIQUE r){
 printf("le théme du rubrique est: %s\n",r.Théme);
-printf("la date de poste du premier rubrique est: %s\n",r.Date_poste);
+printf("la date de poste du premier rubrique est: %s/%s/%s/\n",r.Date_poste.jour,r.Date_poste.mois,r.Date_poste.annee);
 printf("le  premier message est: %s\n",r.Message);
 printf("les liste internets sont: %s\n",r.Liste_internet);
 }
