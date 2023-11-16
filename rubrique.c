83e178e93c1ebbc03f8d2169c864b0ccd5a505f2
cutiegirl02 <150730195+cutiegirl02@users.noreply.github.com>
2023-11-16 18:59:26 +0100

    Add files via upload
    c'est l'affichage de la structure rubrique

---
+++ rubrique.c
@@ -0,0 +1,9 @@
+
+
+void affichage(RUBRIQUE r){
+printf("le théme du rubrique est: %s\n",r.Théme);
+printf("la date de poste du premier rubrique est: %s\n",r.Date_poste);
+printf("le  premier message est: %s\n",r.Message);
+printf("les liste internets sont: %s\n",r.Liste_internet);
+}
+
