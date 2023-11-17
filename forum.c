2eba80c94efb5aa92ecf81258861b0990350b7a1
cutiegirl02 <150730195+cutiegirl02@users.noreply.github.com>
2023-11-17 16:17:21 +0100

    Add files via upload
    j'ai fait la saisie de l'adresse internet du  forum

--- forum.c
+++ forum.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "forum.h"
 
+#define MAX_STRING_LENGTH 100
+
+void saisir_forum(FORUM f){
+
+    while (1){ 
+
+        char adresse_entree[MAX_STRING_LENGTH];
+
+        printf("Donner l'adresse internet du forum: ");
+        scanf("%s", &adresse_entree);
+        
+        if(strcmp(adresse_entree, f.Adresse_internet) != 0){
+            break;
+        }
+    
+    }
+    
+
+}
