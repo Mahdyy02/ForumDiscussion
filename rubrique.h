36ae908cc0a686b8520b06c1641da33a6de59755
cutiegirl02 <150730195+cutiegirl02@users.noreply.github.com>
2023-11-29 19:45:08 +0100

    Add files via upload
    prototypage de la fonction sauvegarder rubrique

--- rubrique.h
+++ rubrique.h
@@ -1,19 +1,21 @@
 #ifndef RUBRIQUE_H_INCLUDED
 #define RUBRIQUE_H_INCLUDED
 #include "date.h"
+#include "message.h"
 
 typedef struct{
     char* Theme;
     Date Date_de_poste;
     unsigned int Numero_inscription;
-    char* Message;
+    unsigned int Nombre_messages;
     char** Liste_internet;
     unsigned int Numero_de_sites;
-
 
 }RUBRIQUE;
 
 void affichage_rubrique(RUBRIQUE);
 void saisir_rubrique(RUBRIQUE*);
+void sauvegarder_message(MESSAGES m, RUBRIQUE r);
+void sauvegarder_rubrique(MESSAGES m , RUBRIQUE r);
 
 #endif
